cf860a83af84d07e3b212e71f6af6c8e12949b38
prima23 <prima.aulia333@gmail.com>
2023-07-23 11:38:19 +0700

    add doc app

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
-
-
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -15,7 +12,6 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Welni Mustika application." });
 });
@@ -24,32 +20,29 @@ require('./app/routes/user.routes')(app);
 require('./app/routes/barang.routes')(app);
 require('./app/routes/penjualan.routes')(app);
 
-
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
+const db = require("./app/models");
+const Role = db.role;
+const User = db.user;
+var bcrypt = require("bcryptjs");
 
-// const db = require("./app/models");
-// const Role = db.role;
-// const User = db.user;
-// var bcrypt = require("bcryptjs");
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
 
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Db');
-//   initial();
-// });
-
-
-// function initial() {
-//   Role.create({
-//     id: 1,
-//     name: "Super_Admin"
-//   });
+function initial() {
+  Role.create({
+    id: 1,
+    name: "Super_Admin"
+  });
  
-//   Role.create({
-//     id: 2,
-//     name: "Admin"
-//   });
-// }
+  Role.create({
+    id: 2,
+    name: "Admin"
+  });
+}
